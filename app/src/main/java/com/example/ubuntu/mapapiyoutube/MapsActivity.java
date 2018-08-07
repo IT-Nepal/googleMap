c663e49fa4062762dbfe70d80e5bae61ee66d2e7
bucky <bucky123@gmail.com>
2018-08-07 12:28:20 +0545

    we ask for permission in run time and we get it

--- app/src/main/java/com/example/ubuntu/mapapiyoutube/MapsActivity.java
+++ app/src/main/java/com/example/ubuntu/mapapiyoutube/MapsActivity.java
@@ -7,6 +7,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.GoogleMap;
@@ -18,8 +19,10 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onMapReady(GoogleMap googleMap){
         Toast.makeText(this,"Map is ready",Toast.LENGTH_LONG).show();
         mMap = googleMap;
-    }
 
+        Log.d(TAG,"map is ready");
+    }
+    String TAG = "MapsActivity";
     private static final String  FineLocation = Manifest.permission.ACCESS_FINE_LOCATION;
     private static final String CourseLocation = Manifest.permission.ACCESS_COARSE_LOCATION;
     private static final int LocationPermissionRequestCode = 123;
@@ -29,12 +32,15 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        getLocationPermission();
     }
 //   SupportMapFragment:- A Map component in an app.
 // This fragment is the simplest way to place a map in an application.
 // It's a wrapper around a view of a map to automatically handle the necessary life cycle needs.
 // Being a fragment, this component can be added to an activity's layout file simply with the XML below.
     public void initMap() {
+        Log.d(TAG,"initMap");
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapFragment);
         mapFragment.getMapAsync(MapsActivity.this);
 //            getMapAsync(OnMapReadyCallback). This class automatically initializes the maps system
@@ -45,7 +51,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     }
     private void getLocationPermission(){
-
+        Log.d(TAG,"getLocationPermission");
     String permission[] = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
 //    coarse location is for network provider's location
 //    and fine location is for both gps provider and network location provider.
@@ -54,12 +60,15 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             if(ContextCompat.checkSelfPermission(getApplicationContext(),CourseLocation)
             == PackageManager.PERMISSION_GRANTED){
             mLocationPermissionGranted = true;
+                Log.d(TAG,"mLocationPermissionGranted is true");
             } else{
                 //ask for permission
+                Log.d(TAG,"ask for permission");
                 ActivityCompat.requestPermissions(this, permission,LocationPermissionRequestCode);
             }
 
         }else{
+            Log.d(TAG,"ask for permission1");
             ActivityCompat.requestPermissions(this, permission,LocationPermissionRequestCode);
         }
     }
@@ -74,11 +83,14 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 for(int i = 0 ; i<grantResults.length; i++){
                     if(grantResults[i] != PackageManager.PERMISSION_GRANTED){
                         mLocationPermissionGranted = false;
+                        Log.d(TAG,"ask for permission and didn't get permission");
                         return ;
                     }
                 }
                 mLocationPermissionGranted = true;
                 //initalize our map
+                initMap();
+                Log.d(TAG,"initmap");
             }
         }
     }
