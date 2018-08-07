4af677b02990990dc2e43b5978f898f54bc8e756
bucky <bucky123@gmail.com>
2018-08-07 13:42:30 +0545

    code for getting current location

--- app/src/main/java/com/example/ubuntu/mapapiyoutube/MapsActivity.java
+++ app/src/main/java/com/example/ubuntu/mapapiyoutube/MapsActivity.java
@@ -2,6 +2,7 @@ package com.example.ubuntu.mapapiyoutube;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -10,15 +11,25 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     public void onMapReady(GoogleMap googleMap){
         Toast.makeText(this,"Map is ready",Toast.LENGTH_LONG).show();
         mMap = googleMap;
+
+        if(mLocationPermissionGranted){
+            getDeviceLocation();
+        }
 
         Log.d(TAG,"map is ready");
     }
@@ -28,13 +39,54 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     private static final int LocationPermissionRequestCode = 123;
     private Boolean mLocationPermissionGranted = false;
     private GoogleMap mMap;
+    private static final float DEFAULT_ZOOM = 15f;
+    private FusedLocationProviderClient mfusedLocationProviderclient; //to get device current location
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
         getLocationPermission();
+
+        if(ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+return;
+        }mMap.setMyLocationEnabled(true);
     }
+
+    public void getDeviceLocation(){
+        Log.d(TAG,"getDeviceLocation : getting the device current location");
+        mfusedLocationProviderclient = LocationServices.getFusedLocationProviderClient(this);
+        try{
+            if(mLocationPermissionGranted){
+                Task location = mfusedLocationProviderclient.getLastLocation();
+                location.addOnCompleteListener(new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if(task.isSuccessful()){
+                            Log.d(TAG,"found location");
+                            Location currentLocation = (Location) task.getResult();
+
+                            moveCamera(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()
+                            ),DEFAULT_ZOOM);
+
+                        }else{
+                            Log.d(TAG,"not found location");
+                            Toast.makeText(MapsActivity.this, "unable to get current location", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+
+        }catch(SecurityException e){
+        Log.e(TAG,"getDeviceLocation: securityException:" + e.getMessage());
+        }
+    }
+private void moveCamera(LatLng latLng, float zoom){
+    Log.d(TAG, "moveCamera: moving the camera to : lat: "+ latLng.latitude + ", lng: "+ latLng.longitude);
+    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,zoom));
+}
 //   SupportMapFragment:- A Map component in an app.
 // This fragment is the simplest way to place a map in an application.
 // It's a wrapper around a view of a map to automatically handle the necessary life cycle needs.
@@ -61,6 +113,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             == PackageManager.PERMISSION_GRANTED){
             mLocationPermissionGranted = true;
                 Log.d(TAG,"mLocationPermissionGranted is true");
+                initMap();
             } else{
                 //ask for permission
                 Log.d(TAG,"ask for permission");
